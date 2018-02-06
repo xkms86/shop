ecc675102c16447a1590a922333ec6dcab3583ee
Krzysztof Stachura <stachura.kms@o2.pl>
2018-02-06 16:38:06 +0100

    New project add

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public void Index()
+        {
+            
+
+
+        }
+
+        public ActionResult Display(int id)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.Name = "Przełącznik ON-OFF";
+            product.Description = "Przełącznik samochodowy, czarny";
+            product.Price = 3;
+
+            if (product.Id == id)
+                return View(product);
+            else
+                return RedirectToAction("Index", "Manage");
+                    }
+    }
+}
---
+++ Shop/Scripts/redScript.js
@@ -0,0 +1,5 @@
+﻿$(function () {
+    $('.red').click(function () {
+        alert("UWAGA! Kliknąłęś w czerwone");
+    });
+});
